e07e88e66c881910455d26eb537838f9382b6c18
ReyDonovan <gennady.zamalaev@gmail.com>
2018-12-14 02:43:31 +0300

    fix prev commit

--- src/server/scripts/World/achievement_scripts.cpp
+++ src/server/scripts/World/achievement_scripts.cpp
@@ -14,7 +14,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
+#include "ScriptMgr.h"
 #include "BattlegroundIC.h"
 #include "Creature.h"
 #include "Player.h"
